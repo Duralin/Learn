f01c061700e1a9c51627f4ffb0d2609e18e96ada
Sayf <duralin@pm.me>
2021-07-18 12:26:00 +0300

    Выполнено задание 4

--- address-book-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreateTests.java
+++ address-book-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreateTests.java
@@ -12,12 +12,16 @@ public class GroupCreateTests {
   public void setUp() throws Exception {
     webDriver = new FirefoxDriver();
     webDriver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    login("admin", "secret");
+  }
+
+  private void login(String username, String password) {
     webDriver.get("http://localhost/addressbook/");
     webDriver.findElement(By.name("user")).click();
     webDriver.findElement(By.name("user")).click();
-    webDriver.findElement(By.name("user")).sendKeys("admin");
+    webDriver.findElement(By.name("user")).sendKeys(username);
     webDriver.findElement(By.name("pass")).click();
-    webDriver.findElement(By.name("pass")).sendKeys("secret");
+    webDriver.findElement(By.name("pass")).sendKeys(password);
     webDriver.findElement(By.id("LoginForm")).click();
     webDriver.findElement(By.xpath("//input[@value='Login']")).click();
   }
@@ -25,22 +29,39 @@ public class GroupCreateTests {
   @Test
   public void testGroupCreation() throws Exception {
 
-    webDriver.findElement(By.linkText("groups")).click();
-    webDriver.findElement(By.name("new")).click();
+    gotoGroupPage();
+    initGroupCreation();
+    fillGroupForm(new GroupData("test1", "test2", "test3"));
+    submitGroupCreation();
+    returnToGroupPage();
+  }
+
+  private void returnToGroupPage() {
+    webDriver.findElement(By.linkText("group page")).click();
+  }
+
+  private void submitGroupCreation() {
+    webDriver.findElement(By.name("submit")).click();
+  }
+
+  private void fillGroupForm(GroupData groupData) {
     webDriver.findElement(By.name("group_name")).click();
     webDriver.findElement(By.name("group_name")).clear();
-    webDriver.findElement(By.name("group_name")).sendKeys("test1");
+    webDriver.findElement(By.name("group_name")).sendKeys(groupData.getName());
     webDriver.findElement(By.name("group_header")).click();
     webDriver.findElement(By.name("group_header")).clear();
-    webDriver.findElement(By.name("group_header")).sendKeys("test2");
+    webDriver.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
     webDriver.findElement(By.name("group_footer")).click();
     webDriver.findElement(By.name("group_footer")).clear();
-    webDriver.findElement(By.name("group_footer")).sendKeys("test3");
-    webDriver.findElement(By.name("submit")).click();
-    webDriver.findElement(By.linkText("group page")).click();
-    webDriver.findElement(By.linkText("Logout")).click();
-    webDriver.findElement(By.name("user")).clear();
-    webDriver.findElement(By.name("user")).sendKeys("admin");
+    webDriver.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
+  }
+
+  private void initGroupCreation() {
+    webDriver.findElement(By.name("new")).click();
+  }
+
+  private void gotoGroupPage() {
+    webDriver.findElement(By.linkText("groups")).click();
   }
 
   @AfterClass(alwaysRun = true)
