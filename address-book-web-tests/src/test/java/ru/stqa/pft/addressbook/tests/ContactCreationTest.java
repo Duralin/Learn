396327c2091ef01b58777b40090bece57efb9915
Sayf <duralin@pm.me>
2021-07-20 16:30:49 +0300

    Сделан третий блок

--- address-book-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
+++ address-book-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
@@ -7,9 +7,7 @@ import ru.stqa.pft.addressbook.model.TestBase;
 public class ContactCreationTest extends TestBase {
   @Test
   public void testContactCreation() throws Exception {
-    app.getContactHelper().iniContactCreation();
-    app.getContactHelper().fillContactField(new ContactData("Ivan", "Ivanov", null, null, "spacegroup", "152135", "152136", "152437", "katalonec@mail.ru", "have no foto", "railway.com", "27", "September", "1999"));
-    app.getContactHelper().returnToContactInfo();
+    app.getContactHelper().createAContact(new ContactData("Ivan", "Ivanov", "Maksimovich", "imaks", "spacegroup", "152135", "152136", "152437", "katalonec@mail.ru", "have no foto", "railway.com", "27", "September", "1999"));
     app.getNavigationHelper().gotoHomepage();
   }
 }
