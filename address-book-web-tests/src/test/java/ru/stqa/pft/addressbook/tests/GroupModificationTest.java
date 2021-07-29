689570ebad1a0bd943276a25aef7f919d17c881c
Sayf <duralin@pm.me>
2021-07-29 17:24:36 +0300

    Работа с группами теперь связана с бд

--- address-book-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTest.java
+++ address-book-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTest.java
@@ -21,21 +21,21 @@ public class GroupModificationTest extends TestBase {
 
     @BeforeMethod
     public void ensurePreconditions(){
-        app.goTo().groupPage();
-        if (app.group().all().size() == 0){
+        if (app.db().groups().size() == 0) {
+            app.goTo().groupPage();
             app.group().create(new GroupData().withName("test1"));
+            }
         }
-    }
 
     @Test
     public void TestGroupModification(){
-        Groups before = app.group().all();
+        Groups before = app.db().groups();
         GroupData modifiedGroup = before.iterator().next();
-        GroupData group = new GroupData().withId(modifiedGroup.getId()).withName("test1");
+        GroupData group = new GroupData().withId(modifiedGroup.getId()).withName("test1").withHeader("test2").withFooter("test3");
+        app.goTo().groupPage();
         app.group().modify(group);
-        Groups after = app.group().all();
-        Assert.assertEquals(after.size(), before.size());
-
+        assertThat(app.group().elementsCount(), equalTo(before.size()));
+        Groups after = app.db().groups();
         assertThat(after, equalTo(before.without(modifiedGroup).withAdded(group)));
     }
 
