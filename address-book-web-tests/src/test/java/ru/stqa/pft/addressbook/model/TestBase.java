b2a55b6d4776b2b3c58c524c8c006a449f60df09
Sayf <duralin@pm.me>
2021-08-02 17:25:25 +0300

    Добавил Allure

--- address-book-web-tests/src/test/java/ru/stqa/pft/addressbook/model/TestBase.java
+++ address-book-web-tests/src/test/java/ru/stqa/pft/addressbook/model/TestBase.java
@@ -1,19 +1,20 @@
 package ru.stqa.pft.addressbook.model;
 
 import org.openqa.selenium.remote.BrowserType;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
+import org.testng.ITestContext;
+import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
+import ru.stqa.pft.addressbook.tests.MyTestListener;
 
+@Listeners(MyTestListener.class)
 public class TestBase {
 
     protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
     @BeforeSuite(alwaysRun = true)
-    public void setUp() throws Exception {
+    public void setUp(ITestContext context) throws Exception {
         app.init();
+        context.setAttribute("app", app);
     }
 
     @AfterSuite(alwaysRun = true)
